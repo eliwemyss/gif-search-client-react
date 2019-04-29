2632ce057e63711e04f53bc1fa0eb90e12418338
Eli Wemyss <eliwemyss@gmail.com>
2019-04-29 14:11:25 -0700

    forgot image alt

--- src/components/GifCard.js
+++ src/components/GifCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const GifCard = (image) => {
     return (
         <div className="gif-card">
-            <img src={image.gif.images.fixed_width.url} alt="" />
+            <img src={image.gif.images.fixed_width.url} alt={image.gif.title} />
         </div>
     )
 };
